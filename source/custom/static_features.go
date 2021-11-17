marquiz/node-feature-discovery

pkg/apis/nfd: work around issues with k8s deepcopy-gen
Without this hack the generated code does not compile.

source/custom/static_features.go
@@ -31,7 +31,9 @@ func getStaticFeatureConfig() []FeatureSpec {
 				LegacyRule{
 					PciID: &rules.PciIDRule{
 						nfdv1alpha1.MatchExpressionSet{
-							"vendor": nfdv1alpha1.NewMatchExpression(nfdv1alpha1.MatchIn, "15b3"),
+							Expressions: nfdv1alpha1.Expressions{
+								"vendor": nfdv1alpha1.NewMatchExpression(nfdv1alpha1.MatchIn, "15b3"),
+							},
 						},
 					},
 				},
@@ -43,8 +45,10 @@ func getStaticFeatureConfig() []FeatureSpec {
 				LegacyRule{
 					LoadedKMod: &rules.LoadedKModRule{
 						nfdv1alpha1.MatchExpressionSet{
-							"ib_uverbs": nfdv1alpha1.NewMatchExpression(nfdv1alpha1.MatchExists),
-							"rdma_ucm":  nfdv1alpha1.NewMatchExpression(nfdv1alpha1.MatchExists),
+							Expressions: nfdv1alpha1.Expressions{
+								"ib_uverbs": nfdv1alpha1.NewMatchExpression(nfdv1alpha1.MatchExists),
+								"rdma_ucm":  nfdv1alpha1.NewMatchExpression(nfdv1alpha1.MatchExists),
+							},
 						},
 					},
 				},

converged-computing/nfd-source

source/custom/static_features.go
@@ -32,7 +32,9 @@ func getStaticFeatureConfig() []CustomRule {
 					{
 						PciID: &rules.PciIDRule{
 							MatchExpressionSet: nfdv1alpha1.MatchExpressionSet{
-								"vendor": nfdv1alpha1.MustCreateMatchExpression(nfdv1alpha1.MatchIn, "15b3"),
+								Expressions: nfdv1alpha1.Expressions{
+									"vendor": nfdv1alpha1.MustCreateMatchExpression(nfdv1alpha1.MatchIn, "15b3"),
+								},
 							},
 						},
 					},
@@ -46,8 +48,10 @@ func getStaticFeatureConfig() []CustomRule {
 					{
 						LoadedKMod: &rules.LoadedKModRule{
 							MatchExpressionSet: nfdv1alpha1.MatchExpressionSet{
-								"ib_uverbs": nfdv1alpha1.MustCreateMatchExpression(nfdv1alpha1.MatchExists),
-								"rdma_ucm":  nfdv1alpha1.MustCreateMatchExpression(nfdv1alpha1.MatchExists),
+								Expressions: nfdv1alpha1.Expressions{
+									"ib_uverbs": nfdv1alpha1.MustCreateMatchExpression(nfdv1alpha1.MatchExists),
+									"rdma_ucm":  nfdv1alpha1.MustCreateMatchExpression(nfdv1alpha1.MatchExists),
+								},
 							},
 						},
 					},